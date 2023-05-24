471400b11db2e7f5b61d92189ad204e44eb947a5
Swayamsiddha das <swayamsiddha.das@corp.knowledgelens.com>
2023-05-24 18:31:41 +0530

    added new changes

--- mqtt/subscriber.py
+++ mqtt/subscriber.py
@@ -1,8 +1,10 @@
 """"paho.mqtt used to create mqtt client"""
+
+
 import paho.mqtt.client as mqtt
 
 def on_message(client,userdata,message):
-    print(f"Received message : , {message.payload.decode()}")
+    print(f"Received message :  {message.payload.decode()}")
 
 
 client = mqtt.Client()
@@ -12,6 +14,6 @@ client.on_message = on_message
 
 client.connect("localhost", 1883)
 
-client.subscribe("IOT")
+client.subscribe("swayam")
 
 client.loop_forever()
